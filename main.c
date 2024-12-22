98f914f33f30a3de8fd83fdef59d4b0713966507
rukshan-prog <rukshanprog@gmail.com>
2024-12-22 22:22:40 +0530

    remove test code in main.c

--- main.c
+++ main.c
@@ -9,29 +9,9 @@
 
 int main(void) {
 
-    /*
-    char *str = getString("Enter Your Name");
-    system("cls");
-    */
-
     Skill *skills = getSkill();
-    /*for (int i = 0; i < 3; i++) {
-        printf("Skill name: %s\n", skills[i].skillName);
-        printf("Skill level: %s\n", skills[i].level);
-    }*/
 
-    /*printf("get skill end\n");
+    char *passions = getPassion();
 
-    char** passion = (char **) getPassion();
-    for (int i = 0; i < 3; i++) {
-        printf("%s\n", passion[i]);
-    }
-
-    printf("get passion end\n");*/
-
-    while (1) {
-        int i;
-        scanf("%d", &i);
-    }
     return 0;
 }
--- src/input/passion.c
+++ src/input/passion.c
@@ -2,57 +2,19 @@
 // Created by [your name] on [date].
 //
 
-
 #include <stdio.h>
 #include <input.h>
 #include <string.h>
 #include <stdlib.h>
 
 
-
-/*
- *
-// how to get string
-char *str = getString("Enter a string");
-printf("You entered : %s\n", str);
-
-// how to get integer
-int integer = getInteger("Enter a number");
-printf("Your entered : %d\n", integer);
-
-// how to get choice
-char* options[] = { // array of options
-        "Option 1",
-        "Option 2",
-        "Option 3"
-};
-int numOptions = 3;  // number of options
-char* choice = getChoice(options, numOptions);
-printf("You selected: %s\n", choice);
- *
- */
-
-
-/* create an array
- * add 1 or more passions to the array (max 3)
- * use getString, getChoice and getInteger function to get the skill from the user
- * return the array
- */
-
 // get passion from the user
 char* getPassion() {
 
-    static char* passions[3] = {
-            "passion 1",
-            "passion 2",
-            "passion 3"
-    };
 
-    // code here
+    getString("Enter Your Passion");
 
 
 
-
-    return (char *) passions;
+    return NULL;
 }
-
