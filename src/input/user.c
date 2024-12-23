28e073f088411cf5ead789e88313518acb51c347
CHAMA COMPUTERS <Sdilum435@gmail.com>
2024-12-23 14:11:18 +0530

    user name and age input code complete

--- src/input/user.c
+++ src/input/user.c
@@ -1,19 +1,34 @@
 # include <stdio.h>
 #include <input.h>
 #include <color.h>
-
-
 char* getUserData() {
+    // Prompt user for name
+    char *user_name = getString("Enter Your Name");
+
+    // Print success message with user's name
+    printf("%sYou entered your name : %s%s\n\n", SUCCESS, user_name, RESET);
+
+    int user_age;
+
+    // Loop until a valid age is entered
+    while (1) {
+        // Prompt user for age
+        user_age = getInteger("Enter Your Age");
+
+        // Check if age is within valid range
+        if (40 >= user_age && user_age >= 10) {
+            // Print success message with user's age
+            printf("%sYou entered your age : %d%s\n\n", SUCCESS, user_age, RESET);
+            break;
+        }else {
+            printf("%s Invalid age please enter an age between 10 and 40  %s\n\n", ERROR, RESET);
 
 
-    char *name = getString("Enter your name");
-    printf("%s\n", name);
-
-
-    return name;
+        }
+    }
 }
 
-char* getUserName() {
-    char *name = getString("Enter your name");
-    return name;
-}
+
+
+
+
